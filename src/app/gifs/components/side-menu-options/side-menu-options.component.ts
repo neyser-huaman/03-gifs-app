ccdaa3bfe74371205b0f1f96dc2c5e7ddc84b3fb
neyser-huaman <neyser@outlook.com>
2025-07-17 03:43:08 +0200

    Activando RouterLink

--- src/app/gifs/components/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu-options/side-menu-options.component.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+
+interface MenuOption{
+  icon: string,
+  label: string,
+  route: string,
+  sublabel: string
+}
 
 @Component({
   selector: 'app-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.component.html',
 })
-export class SideMenuOptionsComponent { }
+export class SideMenuOptionsComponent {
+
+  menuOptions:MenuOption[] = [
+    {
+    icon: 'fa-solid fa-chart-line',
+    label: 'Trending',
+    sublabel: 'Gifs Populares',
+    route: '/dashboard/treding'
+    },
+    {
+    icon: 'fa-solid fa-magnifying-glass',
+    label: 'Buscador',
+    sublabel: 'Buscar Gifs',
+    route: '/dashboard/search'
+    }
+  ]
+
+
+ }
